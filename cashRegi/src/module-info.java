4b669842f62753b3ba5c2af4ec3928eb2cfd8a98
natsuki.kawamoto <prgrm.ntk5@gmail.com>
2023-11-14 20:26:34 +0900

    cashRegi add

---
+++ cashRegi/src/module-info.java
@@ -0,0 +1,6 @@
+module cashRegi {
+	requires javafx.controls;
+	requires javafx.fxml;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
---
+++ sample2/src/module-info.java
@@ -0,0 +1,6 @@
+module sample2 {
+	requires javafx.controls;
+	requires javafx.fxml;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
